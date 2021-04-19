3e25090a47a63be3755bb7639b6ac6c8429fe98a
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-04-19 15:55:41 -0300

    COMMIT home_page

--- devquiz/lib/splash/splash_page.dart
+++ devquiz/lib/splash/splash_page.dart
@@ -1,8 +1,19 @@
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
